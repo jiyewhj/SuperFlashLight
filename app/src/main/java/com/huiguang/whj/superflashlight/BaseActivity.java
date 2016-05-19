fba91b6cadfbd04e09a7eca766a65217eb8360cc
whj <whj871021@163.com>
2016-05-19 13:54:58 +0800

    20160519

--- app/src/main/java/com/huiguang/whj/superflashlight/BaseActivity.java
+++ app/src/main/java/com/huiguang/whj/superflashlight/BaseActivity.java
@@ -2,15 +2,22 @@ package com.huiguang.whj.superflashlight;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 /**
  * Created by Administrator on 2016/5/18.
  */
 public class BaseActivity extends Activity {
+    protected ImageView mImageViewFlashlight;
+    protected ImageView mImageViewFlashlightController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageViewFlashlight = (ImageView) findViewById(R.id.imageView_flashlight);
+        mImageViewFlashlightController = (ImageView) findViewById(R.id.imageView_flashlight_controller);
     }
 }
--- app/src/main/java/com/huiguang/whj/superflashlight/FlashLight.java
+++ app/src/main/java/com/huiguang/whj/superflashlight/FlashLight.java
@@ -1,9 +1,38 @@
 package com.huiguang.whj.superflashlight;
 
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
+
 /**
  * Created by Administrator on 2016/5/18.
  */
 public class FlashLight extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        LayoutParams laParams = mImageViewFlashlightController.getLayoutParams();
+        laParams.height = point.y * 3 / 4;
+        laParams.width = point.x / 3;
+        mImageViewFlashlightController.setLayoutParams(laParams);
+
+
+    }
+
+    public void onClick_Flashlight(View view) {
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            Toast.makeText(this, "没有闪光灯", Toast.LENGTH_LONG).show();
+            return;
+        }
+    }
 
 
 }
